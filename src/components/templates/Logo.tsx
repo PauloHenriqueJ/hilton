fd1807d6b187f870a69a0b8f9bf8fc1a36b99b0c
PauloHenriqueJ <ph.jesus@gmail.com>
2024-10-14 22:33:17 -0300

    hilton

--- src/components/templates/Logo.tsx
+++ src/components/templates/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo(){
     return(
       <div className="flex items-center">
         <div className="fixed left-5 flex justify-start items-center">
-        <span className=" lg:text-5xl md:text-2xl sm:text-xl">Bem vindo</span>
+        <span className=" lg:text-5xl md:text-2xl">Bem vindo</span>
       <Image src="https://imgur.com/h3V3ClZ.png" alt="logo"
       width={420}
       height={200}
