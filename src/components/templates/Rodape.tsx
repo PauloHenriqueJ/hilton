9ae0ba51b54391a6557254eb2dd7a5d2acfabd91
PauloHenriqueJ <ph.jesus@gmail.com>
2024-10-16 20:31:07 -0300

    linkPortfolio

--- src/components/templates/Rodape.tsx
+++ src/components/templates/Rodape.tsx
@@ -1,4 +1,4 @@
-import Contatos from "../Contatos"
+import Link from 'next/link';
 
 interface RodapeProps{
     className?: string
@@ -14,6 +14,11 @@ export default function Rodape (props:RodapeProps){
                 <div className="flex justify-end items-center boxed">
                
                 Todos os direitos reservados &copy; {new Date().getFullYear()}
+                <Link href="https://www.devph.com.br" legacyBehavior>
+              <a target="_blank" rel="noopener noreferrer" className="underline ml-2 hover:text-blue-400">
+             Portfólio desenvolvedor
+              </a>
+            </Link>
                 </div>
                
                  </footer>
