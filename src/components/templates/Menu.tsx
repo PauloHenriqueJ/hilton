a70c453ba116d35a514343fab78d869bcb1b7d56
PauloHenriqueJ <ph.jesus@gmail.com>
2024-10-15 22:47:24 -0300

    aju

--- src/components/templates/Menu.tsx
+++ src/components/templates/Menu.tsx
@@ -6,8 +6,8 @@ interface MenuProps{
 
 export default function Menu (props: MenuProps){
     return (
-        <aside className={` ${props.className  ?? ''}` }>
-            <nav className="fixed top-0 left-0 w-full flex justify-center items-center space-x-3 bg-white py-2 shadow-md boxed">
+        <aside className={` boxed ${props.className  ?? ''}` }>
+            <nav className="fixed top-0 left-0 w-full flex justify-center items-center space-x-3 bg-white py-2 shadow-md boxed sm:block">
     <Link className="transition-transform hover:scale-105 hover:font-semibold " href={"#servicos"}>Serviços</Link>
     <Link className="transition-transform hover:scale-105 hover:font-semibold " href={"#projetos"}>Projetos</Link>
     <Link className="transition-transform hover:scale-105 hover:font-semibold " href={"#"}>Sobre nós</Link>
