5ef8b9cdbf19a43bee382b20abab08f67edd8349
PauloHenriqueJ <ph.jesus@gmail.com>
2024-10-16 19:59:33 -0300

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,22 @@ import {Montserrat} from 'next/font/google'
 export const metadata: Metadata = {
   title: "HM Pinturas",
   description: "Pintamos com excelência, transformamos com paixão",
+  keywords: ["pintura", "revitalizar", "Serviço pintura", "HM Pinturas"],
+  openGraph: {
+    type: "website",
+    url: "https://hm-pinturas.vercel.app/",
+    title: "HM Pinturas",
+    description: "Pintamos com excelência, transformamos com paixão",
+    images: [
+      {
+        url: "https://imgur.com/h3V3ClZ.png",
+        width: 800,
+        height: 600,
+        alt: "Imagem do lar pintado",
+      },
+      
+    ],
+  }
 };
 
 const font = Montserrat ({
