4aa005bb3745e99a0132d257fde0cf3dd5f3f52b
PauloHenriqueJ <ph.jesus@gmail.com>
2024-10-17 00:31:07 -0300

    semPagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
-import Pagina from "@/components/Pagina";
+import Contatos from "@/components/Contatos";
+import Projetos from "@/components/Grid";
+import Sobre from "@/components/Sobre";
+import Cabecalho from "@/components/templates/Cabecalho";
+import Rodape from "@/components/templates/Rodape";
+import Servicos from "@/components/templates/Servicos";
+import { ReactNode } from "react";
 
 
-export default function Home() {
+interface PaginaProps {
+  children?: ReactNode;
+}
+
+export default function Pagina(props: PaginaProps) {
   return (
-    <Pagina >
-     <main className="items-center">
-      <div id="servicos" className=" items-start justify-center">
-      
-
+    <div className="flex flex-col min-h-screen no-scrollbar">
+      <Cabecalho className="flex flex-col items-center justify-center w-full" />
+      <div className="flex-1 flex justify-center">
+        <main className="flex-1 py-7 mx-auto w-full max-w-screen-lg px-4 sm:px-6 lg:px-8">
+          <Servicos />
+          <Projetos />
+          <Sobre />
+          <Contatos />
+          {props.children}
+        </main>
       </div>
-    
-     </main>
-    </Pagina>
+      <Rodape className="h-16" />
+    </div>
   );
 }
