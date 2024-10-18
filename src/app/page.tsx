251623a99a90db7c54b557a2dc205c88e0d78064
PauloHenriqueJ <ph.jesus@gmail.com>
2024-10-18 17:02:08 -0300

    atualizacao2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
+import Contatos from "@/components/Contatos";
+import Projetos from "@/components/Grid";
+import Sobre from "@/components/Sobre";
+import Cabecalho from "@/components/templates/Cabecalho";
+import Rodape from "@/components/templates/Rodape";
+import Servicos from "@/components/templates/Servicos";
 import { ReactNode } from "react";
-import Pagina from "./Pagina";
-
 
 interface PaginaProps {
-  children?: ReactNode;
-}
+    children?: ReactNode;
+  }
 
-export default function page(props: PaginaProps) {
-  return (
-   <Pagina/>
-  );
+export default function Pagina(props: PaginaProps){
+    return(
+        <div className=" flex-col min-h-screen no-scrollbar">
+            <Cabecalho className="flex flex-col items-center justify-center w-full"/>
+            <div className=" flex-1 flex boxed">
+             
+                <main className="flex-1 py-7">
+                    <Servicos/>
+                    <Projetos/>
+                    <Sobre/>
+                    <Contatos/>
+                    {props.children}</main>
+            </div>
+            <Rodape className="h-16"/>
+        </div> 
+    )
 }
