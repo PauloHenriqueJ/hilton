c92751c036dc2fd87bbafe611128214966fc3166
PauloHenriqueJ <ph.jesus@gmail.com>
2024-10-18 11:51:50 -0300

    AddProject2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ interface PaginaProps {
 
 export default function Pagina(props: PaginaProps) {
   return (
-    <div className="flex flex-col min-h-screen no-scrollbar px-7 lg:px-0">
+    <div className="flex flex-col min-h-screen no-scrollbar">
       <Cabecalho className="flex flex-col items-center justify-center w-full" />
       <div className="flex-1 flex justify-center">
         <main className="flex-1 py-7 mx-auto w-full max-w-screen-lg px-4 sm:px-6 lg:px-8">
--- src/components/Contatos.tsx
+++ src/components/Contatos.tsx
@@ -12,22 +12,22 @@ export default function Contatos(){
        
     
         <div className="flex flex-col sm:flex-row sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4  ">
-        <span className='text-black font-semibold'>(71) 9 92159099</span>
+        <span className='text-black font-semibold'>(71) 9 9215-9099</span>
           <Link href="https://wa.me/5571992159099" legacyBehavior>
             <a target="_blank" rel="noopener noreferrer" className="flex justify-center hover:text-green-400">
-              <FaWhatsapp className='text-black' size={30} />
+              <FaWhatsapp className='text-green-400 ' size={30} />
             </a>
           </Link>
           
           <Link href="https://www.instagram.com/hilton_melo87/" legacyBehavior>
             <a target="_blank" rel="noopener noreferrer" className="flex justify-center hover:text-pink-400">
-              <FaInstagram className='text-black' size={30} />
+              <FaInstagram className='text-violet-700' size={30} />
             </a>
           </Link>
           
           <Link href="https://www.facebook.com/hiltonmelosilva.silva" legacyBehavior>
             <a target="_blank" rel="noopener noreferrer" className="flex justify-center hover:text-blue-400">
-              <FaFacebook className='text-black' size={30} />
+              <FaFacebook className='text-blue-600' size={30} />
             </a>
           </Link>
         </div>
