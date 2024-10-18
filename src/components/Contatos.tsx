33b19913ae1006dced58b6dc3c63b5b8ee1de4cd
PauloHenriqueJ <ph.jesus@gmail.com>
2024-10-18 18:24:23 -0300

    uptadeContato

--- src/components/Contatos.tsx
+++ src/components/Contatos.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link';
-import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaPhone, FaWhatsapp } from 'react-icons/fa';
 
 export default function Contatos(){
     return(
-        <div id='contatos' className=' flex-1 border border-zinc-700 rounded-md '>
+        <div id='contatos' className=' text-center  flexflex-1 border border-zinc-700 rounded-md items-center '>
             <h1 className="antialiased text-center font-bold sm:font-medium mt-7 text-lg sm:text-xl md:text-2xl lg:text-5xl">
+          
         Contatos
       </h1>
+     
+      <span  className=' text-black font-semibold text-center'> (71) 9 9215-9099</span>
+      
       <div>
-      <div className=" text-white p-2 rounded-lg flex flex-col gap-3">
+      <div className=" text-white p-2 rounded-lg  flex gap-3 items-center justify-center">
        
     
-        <div className="flex flex-col sm:flex-row sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4  ">
-        <span className='text-black font-semibold'>(71) 9 9215-9099</span>
+        <div className="flex items-center justify-center sm:flex-row sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4 gap-4 lg:gap-3 ">
+          
+       
           <Link href="https://wa.me/5571992159099" legacyBehavior>
             <a target="_blank" rel="noopener noreferrer" className="flex justify-center hover:text-green-400">
               <FaWhatsapp className='text-green-400 ' size={30} />
