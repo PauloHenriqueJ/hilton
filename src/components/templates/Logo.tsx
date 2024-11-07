58bbbc6966daaf24867a5b785450f4d5729ee77c
PauloHenriqueJ <ph.jesus@gmail.com>
2024-11-06 23:23:35 -0300

    ups

--- src/components/templates/Logo.tsx
+++ src/components/templates/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo(){
     return(
       <div className="flex items-center">
-        <div className="fixed absolute left-5 flex justify-start  items-center">
+        <div className="fixed absolute left-5 flex justify-start  items-center sm: mt-5">
         <div className="flex flex-col">
         <span className=" bg-gradient-to-r from-blue-800 via-green-600 to-yellow-500 bg-clip-text text-transparent text-3xl lg:text-5xl text-grad
         font-bold shadow-sm">
